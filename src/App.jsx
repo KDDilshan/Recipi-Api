66684c35851c1a06c0dc16ba6f938798c030f55e
KDDilshan <kdprojects847@gmail.com>
2024-10-22 09:30:06 +0530

    Added new

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,13 @@ import { useState } from 'react'
 import Serch from './Components/Serch'
 
 function App() {
+  const [FoodData,setFoodData]=useState([])
   return (
     <div className='App'>
-      <Serch />
+      <Serch FoodData={FoodData} setFoodData={setFoodData} />
+      {FoodData.map((food)=>
+      <h1>{food.title}</h1>
+      )}
     </div>
   )
 }
--- src/Components/Serch.jsx
+++ src/Components/Serch.jsx
@@ -4,14 +4,14 @@ const url="https://api.spoonacular.com/recipes/complexSearch"
 const API_KEY="afc1e47d49724ea3bf596228619fdc69"
 
 
-export default function Serch(){
+export default function Serch({FoodData,setFoodData}){
     const [query,setQuery]=useState("Pizza")
     //sytax of use effect hook
     useEffect(()=>{
         async function FetchFood(){
           const res=await fetch(`${url}?query=${query}&apiKey=${API_KEY}`)
           const data=await res.json()
-          console.log(data.results)
+          setFoodData(data.results)
         }
         FetchFood()
     },[query])
