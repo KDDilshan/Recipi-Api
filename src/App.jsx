44224dc53cae5747a7ac276bb7aed5b98f6d31a9
KDDilshan <kdprojects847@gmail.com>
2024-10-22 09:50:22 +0530

    added new

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
 import Serch from './Components/Serch'
 import FoodList from './Components/FoodList'
+import Nav from './Components/Nav'
+import styles from "./App.css"
 
 function App() {
   const [FoodData,setFoodData]=useState([])
   return (
     <div className='App'>
+      <Nav />
       <Serch FoodData={FoodData} setFoodData={setFoodData} />
       <FoodList FoodData={FoodData} />
     </div>
