b4db7cab11ac95f3507c833df755cb7dc7d4edcb
KDDilshan <kdprojects847@gmail.com>
2024-10-22 09:22:19 +0530

    Added new

--- src/Components/Serch.jsx
+++ src/Components/Serch.jsx
@@ -1,13 +1,19 @@
 import { useEffect, useState } from "react"
 
+const url="https://api.spoonacular.com/recipes/complexSearch"
+const API_KEY="afc1e47d49724ea3bf596228619fdc69"
+
+
 export default function Serch(){
     const [query,setQuery]=useState("Pizza")
     //sytax of use effect hook
     useEffect(()=>{
-        function demo(){
-            console.log("Demo funtion executed")
+        async function FetchFood(){
+          const res=await fetch(`${url}?query=${query}&apiKey=${API_KEY}`)
+          const data=await res.json()
+          console.log(data.results)
         }
-        demo()
+        FetchFood()
     },[query])
     return (
         <div>
