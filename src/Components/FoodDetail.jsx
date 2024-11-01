7404448ca8f139952122551d81ee3f693adf4a11
KDDilshan <kdprojects847@gmail.com>
2024-11-01 11:34:50 +0530

    added new items

--- src/Components/FoodDetail.jsx
+++ src/Components/FoodDetail.jsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from "react"
+import styles from "./FoodDeatils.module.css"
+import ItemList from "./ItemList"
 
 export default function FoodDetail({FoodId}){
     const [food,setFood]=useState({})
@@ -19,10 +21,10 @@ export default function FoodDetail({FoodId}){
 
     return (
         <div>
-            <div>
-                <h1>{food.title}</h1>
-                <img src={food.image} alt="" />
-                <div>
+            <div className={styles.recipeCard}>
+                <h1 className={styles.recipeName}>{food.title}</h1>
+                <img className={styles.recipeImage} src={food.image} alt="" />
+                <div className={styles.recipeDeatils}>
                     <span>
                         <strong>⏰{food.readyInMinutes} Minutes</strong>
                     </span>
@@ -30,19 +32,24 @@ export default function FoodDetail({FoodId}){
                         <strong>Serves 😉{food.servings}</strong>
                     </span>
                     <span>
-                        {food.vegetarian ? "🥕Vegetarian":"🥩Non-Vegetarian"}
+                        <strong>{food.vegetarian ? "🥕Vegetarian":"🥩Non-Vegetarian"}</strong>
                     </span>
                     <span>
-                        {food.vegan ? "Vegan":""}
+                        <strong>{food.vegan ? "Vegan":""}</strong>
                     </span>
                 </div>
                 <div>
-                   $ <span>{food.pricePerServing/100} Per Serving</span>
+                   $ <span> <strong>{food.pricePerServing/100} Per Serving</strong></span>
                 </div>
-            </div>
-            <div>
-                <h2>Instructions</h2>
-                {isLoading ? (<p>Loading ...</p>):(food.analyzedInstructions[0].steps.map((step)=>(<li>{step.step}</li>)))}
+
+                    <h2>Ingredients</h2>
+                    <ItemList food={food} isLoading={isLoading}/>
+                <div className={styles.recipeInstructions}>
+                    <ol>
+                    <h2>Instructions</h2>
+                    {isLoading ? (<p>Loading ...</p>):(food.analyzedInstructions[0].steps.map((step)=>(<li>{step.step}</li>)))}
+                    </ol>
+                </div>
             </div>
         </div> 
     )
